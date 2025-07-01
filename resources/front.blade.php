e365e31c11a439864bca22c9341411530a8ff175
randika-cohere <randika@cohereit.com>
2025-07-01 19:36:27 +0530

    added - front view

---
+++ resources/front.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SEVN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body style="background: #000;">
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12" style="height: 100vh;">
+            <div class="d-flex flex-wrap justify-content-center align-items-center h-100">
+                <div class="text-center">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-4">
+                            <img src="{{ asset('sevn-logo-new.png') }}" alt="sevn" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd('Hello from the SEVN API');
-    return view('welcome');
+    return view('front');
 });
